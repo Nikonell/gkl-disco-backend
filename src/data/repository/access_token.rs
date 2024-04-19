e021079e060bf3190c4a1b4b95d9e96376e65d8a
Nikonell <rozhkovn08@gmail.com>
2024-04-19 22:00:53 +0700

    feat: authorize method

--- src/data/repository/access_token.rs
+++ src/data/repository/access_token.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
-use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr, EntityTrait};
-use crate::domain::entities::access_token::{Entity as AccessTokenEntity, Model, ActiveModel};
+use sea_orm::{DatabaseConnection, DbErr, EntityTrait, ColumnTrait, QueryFilter};
+use crate::domain::entities::access_token::{Entity as AccessTokenEntity, Model, Column};
 
 #[derive(Clone)]
 pub struct AccessTokenRepository {
@@ -12,23 +12,10 @@ impl AccessTokenRepository {
         Self { connection }
     }
 
-    pub async fn find_all(&self) -> Result<Vec<Model>, DbErr> {
-        AccessTokenEntity::find().all(self.connection.as_ref()).await
-    }
-
-    pub async fn find_by_id(&self, id: i32) -> Result<Option<Model>, DbErr> {
-        AccessTokenEntity::find_by_id(id).one(self.connection.as_ref()).await
-    }
-
-    pub async fn create(&self, model: ActiveModel) -> Result<Model, DbErr> {
-        model.insert(self.connection.as_ref()).await
-    }
-
-    pub async fn update(&self, model: ActiveModel) -> Result<Model, DbErr> {
-        model.update(self.connection.as_ref()).await
-    }
-
-    pub async fn delete(&self, id: i32) -> Result<u64, DbErr> {
-        AccessTokenEntity::delete_by_id(id).exec(self.connection.as_ref()).await.map(|res| res.rows_affected)
+    pub async fn find_by_token(&self, token: &String) -> Result<Option<Model>, DbErr> {
+        AccessTokenEntity::find()
+           .filter(Column::Token.eq(token))
+           .one(self.connection.as_ref())
+           .await
     }
 }
--- src/domain/services/access_token.rs
+++ src/domain/services/access_token.rs
@@ -1,4 +1,6 @@
 use std::sync::Arc;
+use http_api_problem::HttpApiProblem;
+use reqwest::StatusCode;
 use sea_orm::{DatabaseConnection, DbErr};
 use crate::data::repository::access_token::AccessTokenRepository;
 use crate::domain::entities::access_token::{Model as AccessTokenModel, ActiveModel as AccessTokenActiveModel};
@@ -14,23 +16,10 @@ impl AccessTokenService {
         Self { repository }
     }
 
-    pub async fn get_all_access_tokens(&self) -> Result<Vec<AccessTokenModel>, DbErr> {
-        self.repository.find_all().await
-    }
-
-    pub async fn get_access_token_by_id(&self, id: i32) -> Result<Option<AccessTokenModel>, DbErr> {
-        self.repository.find_by_id(id).await
-    }
-
-    pub async fn create_access_token(&self, model: AccessTokenActiveModel) -> Result<AccessTokenModel, DbErr> {
-        self.repository.create(model).await
-    }
-
-    pub async fn update_access_token(&self, model: AccessTokenActiveModel) -> Result<AccessTokenModel, DbErr> {
-        self.repository.update(model).await
-    }
-
-    pub async fn delete_access_token(&self, id: i32) -> Result<u64, DbErr> {
-        self.repository.delete(id).await
+    pub async fn authorize(&self, token: String) -> Result<(), HttpApiProblem> {
+        match self.repository.find_by_token(&token).await {
+            Ok(Some(_)) => return Ok(()),
+            _ => return Err(HttpApiProblem::new(StatusCode::UNAUTHORIZED).title("Unauthorized")),
+        }
     }
 }
